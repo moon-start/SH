dbcb9d859cbecb00b825adcec847e3e511c455f4
moon-start <login0516mp4@gmail.com>
2020-01-16 03:50:47 +0800

    All File

--- chmod.sh
+++ chmod.sh
@@ -5,9 +5,11 @@ echo `pwd`
 chmod +x *.sh
 
 
-PATH=`pwd`:$PATH:12345
-
-export PATH
+# PATH=`pwd`:$PATH:12345
+# export PATH
 
 ##  nano ~/.bash_profile
-## echo export PATH=`pwd`:$PATH:12345
+# echo export PATH=`pwd`:$PATH:12345
+
+echo export PATH=`pwd`:$PATH>>~/.bash_profile 
+source ~/.bash_profile
