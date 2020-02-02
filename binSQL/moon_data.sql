214228693cbf1a8417a84305679b4bc4a6765ae7
moon-start <login0516mp4@gmail.com>
2020-02-02 12:26:57 +0800

    All File

--- binSQL/moon_data.sql
+++ binSQL/moon_data.sql
@@ -1,6 +1,22 @@
-LOAD DATA INFILE '/usr/bin/Invoice.csv' 
+-- use moon;load data low_priority infile "/usr/bin/Invoice.csv" into table Invoice;
+-- SHOW VARIABLES LIKE "secure_file_priv";
+-- show variables like '%secure%';
+
+-- 預設讀寫目錄
+--  show variables like '%tmpdir%';
+LOAD DATA INFILE '/root/SH/binSQL/Invoice.csv' 
 INTO TABLE Invoice
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+
+use moon;LOAD DATA  INFILE "Invoice.csv" INTO TABLE Invoice;
+
+
+
+
+cd /var/lib/ 
+chown mysql:mysql mysql/ -R
+use moon;LOAD DATA INFILE "/var/lib/mysql-files/Invoice.csv" into table Invoice;
